4199cdaf96c2892e7cef62b7759873b8585e0c72
Zsolt Parragi <zsolt.parragi@cancellar.hu>
2024-12-02 07:47:07 +0000

    SMGR GUC variable and chaining
    The overall goal of this commit is to introduce a user interface to
    the previous SMGR patch.
    
    The idea is to allow a simple configuration for multiple "modificator"
    SMGRs similar to the fsync_checker in the original proposal.
    
    * Extensions should be able to declare a named lists of SMGR implementations,
    also specifying if the given SMGR is an "end" implementation for
    actual storage, or if it is a modifier implementation for some other
    purpose.
    * Users should be able to specify a list of SMGRs: possibly multiple modifiers,
    and one storage implementation at the end to configure how the
    storage manager is constructed.
    
    This commit introduces a new GUC variable, `smgr_chain`, which allows
    users to configure multiple SMGR implementations: it is a comma separated list,
    where the last entry most be a storage implementation, the others must be
    modifiers. The default value of this variable is "md".
    
    The internal storage manager API is also refactored to include an easy
    way for SMGR implementations to support proper chaining. Modifier SMGR
    implementations also only have to implement the functions they actually
    change, and can leave everything else as empty (NULL). And with this
    change we can make the functions of the md smgr static.
    
    The fsync example extension is also modified to match the new API.

--- src/backend/tcop/postgres.c
+++ src/backend/tcop/postgres.c
@@ -4116,6 +4116,8 @@ PostgresSingleUserMain(int argc, char *argv[],
 	 */
 	process_shared_preload_libraries();
 
+	process_smgr_chain();
+
 	/* Initialize MaxBackends */
 	InitializeMaxBackends();
 
